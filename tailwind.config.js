36aca0c9ded949d7700c01100af6e6500d3115b7
Jpeyton <jpwalk319@gmail.com>
2021-06-03 12:01:33 -0600

    added tailwinds to globalcss file and a stylelint setting to ignore the error associated with it

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./components/layout.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
