3f2fc4d0155414a7d0b858d3c3f9b81c46ccae2a
Jpeyton <jpwalk319@gmail.com>
2021-06-07 13:56:29 -0600

    added bootstrap to appjs and fixed header layout

--- components/header.js
+++ components/header.js
@@ -1,15 +1,46 @@
-import Link from 'next/link'
+import Link from "next/link";
+
 
 const Header = () => {
-    return (
-        <nav className='navbar navbar-expand-lg'>
-            <ul className=''>
-                <li className=''><Link href='/DashBoard'><a className='transition duration-500 hover:bg-yellow-500 transition duration-500 hover:tracking-widest p-1 rounded'>DashBoard</a></Link></li>
-                <li className=''>Trance</li>
-                <li className=''>Schedule</li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Logo?      
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link href='/'><a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a></Link>
+            </li>
+            <li className="nav-item">
+              <Link href='/Schedule'><a className="nav-link" href="#">
+                Schedule
+              </a></Link>
+            </li>
+            <li className="nav-item">
+              <Link href='/Trance'><a className="nav-link" href="#">
+                Trance
+              </a></Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
