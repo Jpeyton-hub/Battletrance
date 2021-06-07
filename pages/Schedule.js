1111b19c1497286d74331298aa5a0ee7d4bdef01
Jpeyton <jpwalk319@gmail.com>
2021-06-07 16:13:30 -0600

    forgot to git add

---
+++ pages/Schedule.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+const Schedule = () => {
+    return (
+        <Layout>
+            <h1>hello from schedule page</h1>
+        </Layout>
+    )
+}
+
+export default Schedule
---
+++ pages/Trance.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+const Trance = () => {
+    return (
+        <Layout>
+            <h1>hello from trance page</h1>
+        </Layout>
+    )
+}
+
+export default Trance
